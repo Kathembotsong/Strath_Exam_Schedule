6ba34c464ea6e47941f820849333a2c7009dd484
kathembotsongodieudonne <kathembo.dieudonne@gmail.com>
2024-01-18 13:58:36 +0300

    commit

---
+++ system/read_bbit_3_1.php
@@ -0,0 +1,81 @@
+<?php 
+include 'dbcon.php'; 
+include 'header.php';     
+include 'js_datatable.php';
+
+if(isset($_REQUEST['delete_id'])){
+    $id = $_REQUEST['delete_id']; // get delete_id and store in $id variable
+
+    $select_stmt = $conn->prepare('SELECT * FROM enrollments_bbt WHERE enrol_id =:id'); // sql select query
+    $select_stmt->bindParam(':id',$id);
+    $select_stmt->execute();
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+    // delete an original record from the database
+    $delete_stmt = $conn->prepare('DELETE FROM enrollments_bbt WHERE enrol_id =:id');
+    $delete_stmt->bindParam(':id',$id);
+    $delete_stmt->execute();
+    header("Location:read_bbit_3_1.php");
+}
+
+$group_name = "BBT 3.1";
+$select_stmt = $conn->prepare("SELECT * FROM enrollments_bbt WHERE group_name = :group_name");
+$select_stmt->bindParam(':group_name', $group_name);
+$select_stmt->execute();
+?>
+
+<div class="container-fluid">
+  <div class="row">  
+    <?php include "sidebar.php";?>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="wrapper">    
+            <div class="">            
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h1 style="text-align: center;">BBIT 3.1</h1>
+                            <h3><a href="enrollment_bbt.php" style="text-decoration:none;"><span class="fas fa-plus"></span>&nbsp; New Student</a></h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
+                                    <thead>
+                                        <tr>
+                                            <th style="text-align: center;">Student Code</th>
+                                            <th style="text-align: center;">Subject Code</th>
+                                            <th style="text-align: center;">Subject Name</th>
+                                            <th style="text-align: center;">Group Name</th>
+                                            <th style="text-align: center;">Lecturer Name</th>
+                                            <th style="text-align: center;">Enrol Status</th>
+                                            <th style="text-align: center;">Edit</th>
+                                            <th style="text-align: center;">Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php 
+                                            while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $row['student_code']; ?></td>
+                                            <td><?php echo $row['subject_code']; ?></td>
+                                            <td><?php echo $row['subject_name']; ?></td>
+                                            <td><?php echo $row['group_name']; ?></td>
+                                            <td><?php echo $row['lect_name']; ?></td>
+                                            <td><?php echo $row['enrol_status']; ?></td>
+                                            <td><a href="edit_enrollment_bbt.php?update_id=<?php echo $row['enrol_id']; ?>" class="btn btn-warning"><i class="fas fa-edit"></i></a></td>
+                                            <td><a href="?delete_id=<?php echo $row['enrol_id']; ?>" class="btn btn-danger"><i class="fas fa-trash"></a></td>
+                                        </tr>
+                                        <?php
+                                            }                    
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>          
+        </div>
+    </main>
+    <?php require 'footer.php' ?>
+</div>
+</div>
--- system/sidebar.php
+++ system/sidebar.php
@@ -51,12 +51,45 @@
                       <div class="collapse" id="GroupCollapse" aria-labelledby="headine" data-bs-parent="#sidenavAccordionPages">
                           <nav class="sb-sidenav-menu-nested nav">
                             <li class="nav-link">
-                              <a class="nav-link" style="color:white;" href="read_enrollments_bbit.php">BBIT</a>
-                              <a class="nav-link" style="color:white;" href="read_group_bcom.php">BCOM</a>
-                              <a class="nav-link" style="color:white;" href="read_group_scs.php">SCS</a>
-                              <a class="nav-link" style="color:white;" href="read_group_sls.php">SlS</a>
-                              <a class="nav-link" style="color:white;" href="read_group_tourism.php">TOURISM</a>
-                            </li>                              
+                              <a class="nav-link collapsed" style="color:white;" href="#" data-bs-toggle="collapse" data-bs-target="#BBITGroups" aria-expanded="false" aria-controls="HomeCollapse">
+                              <h4>BBIT</h4>
+                              </a>
+                              <div class="collapse" id="BBITGroups" aria-labelledby="headine" >
+                                <nav class="sb-sidenav-menu-nested nav">
+                                  <li class="nav-link">
+                                    <a class="nav-link" style="color:white;" href="read_bbit_1_1.php">BBIT 1.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_1_2.php">BBIT 1.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_2_1.php">BBIT 2.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_2_2.php">BBIT 2.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_3_1.php">BBIT 3.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_3_2.php">BBIT 3.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_4_1.php">BBIT 4.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bbit_4_2.php">BBIT 4.2</a>
+                                    <a class="nav-link" style="color:red;" href="read_bbit_repeats.php">Repeats&Specials</a>
+                                  </li>                              
+                                </nav>
+                              </div>
+                            </li>
+                            <li class="nav-link">
+                              <a class="nav-link collapsed" style="color:white;" href="#" data-bs-toggle="collapse" data-bs-target="#BCOMGroups" aria-expanded="false" aria-controls="HomeCollapse">
+                              <h4>BCOM</h4>
+                              </a>
+                              <div class="collapse" id="BCOMGroups" aria-labelledby="headine" >
+                                <nav class="sb-sidenav-menu-nested nav">
+                                  <li class="nav-link">
+                                    <a class="nav-link" style="color:white;" href="read_bcom_1_1.php">BCOM 1.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_1_2.php">BCOM 1.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_2_1.php">BCOM 2.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_2_2.php">BCOM 2.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_3_1.php">BCOM 3.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_3_2.php">BCOM 3.2</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_4_1.php">BCOM 4.1</a>
+                                    <a class="nav-link" style="color:white;" href="read_bcom_4_2.php">BCOM 4.2</a>
+                                    <a class="nav-link" style="color:red;" href="read_bcom_repeats.php">Repeats&Specials</a>
+                                  </li>                              
+                                </nav>
+                              </div>
+                            </li>                                    
                           </nav>
                       </div>
                   <ul class="nav-link">
